d323d8aab21a49c5bf2829ec7e49fc4a4a0ad13d
Mobinul-Sekh <mskmd06@gmail.com>
2023-07-17 23:01:48 +0530

    separated signin signup pages

--- routes/auth.js
+++ routes/auth.js
@@ -2,34 +2,67 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 
+router.get("/signin", (req, res) => {
+    res.render('signin');
+})
+
 router.get("/signup", (req, res) => {
-    res.render('authentication');
+    res.render('signup');
 })
 
 router.post("/create-user", (req, res) => {
 
-    const { email, password } = req.body;
+    const { name, email, password } = req.body;
 
     User.findOne({email: email}, (err, foundUser) => {
         if(!foundUser){
             const newUser = new User({
+                name: name,
                 email: email,
                 password: password
             })
 
             newUser.save()
-            res.redirect('/signup')
+            res.redirect('/signin')
         }
         else{
-            console.log('User Already Exists!');
+            const response = {
+                "msg" : "User Already Exists please Sign in!"
+            }
+            res.json(response)
+        }
+    })
+})
 
-            if( foundUser.password === password){
-                res.render("list")
+router.post("/signin-user", (req, res) => {
+
+    const { email, password } = req.body;
+
+    User.findOne({email: email}, (err, foundUser) => {
+        if(!foundUser){
+            const response = {
+                "msg" : "No user registered with this credential, Sign up first!"
+            }
+            // res.redirect('/signup')
+            res.json(response)
+        }
+        else{
+            if(password !== foundUser.password){
+                const response = {
+                    "msg" : "Password did not match!"
+                }
+                // res.redirect('/signin')
+                res.json(response)
             }
             else{
-                console.log('not verified');
-                res.redirect('/signup')
+                const response = {
+                    "msg" : "Sign-In Successful!!"
+                }
+                res.json(response)
             }
+        }
+        if(err){
+            console.log("Sing-in err --> ");
         }
     })
 })
